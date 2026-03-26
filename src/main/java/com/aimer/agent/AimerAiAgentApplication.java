bbd9ba608d40ac6c2e9b10beaae21f06ce2745d9
chenxingyong <2103095120@qq.com>
2026-03-26 19:59:55 +0800

    项目初始化

---
+++ src/main/java/com/aimer/agent/AimerAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.aimer.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimerAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AimerAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aimer/agent/AimerAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimer.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimerAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
