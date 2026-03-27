9107753b0334cee0ebf58d545a85dbde8d063b33
chenxingyong <2103095120@qq.com>
2026-03-27 19:02:25 +0800

    feat: 自定义 Re-Reading Advisor

---
+++ src/main/java/com/aimer/agent/advisor/ReReadingAdvisor.java
@@ -0,0 +1,42 @@
+package com.aimer.agent.advisor;
+
+import org.springframework.ai.chat.client.advisor.api.*;
+import reactor.core.publisher.Flux;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReReadingAdvisor implements CallAroundAdvisor, StreamAroundAdvisor {
+    @Override
+    public AdvisedResponse aroundCall(AdvisedRequest advisedRequest, CallAroundAdvisorChain chain) {
+        return chain.nextAroundCall(this.before(advisedRequest));
+    }
+
+    @Override
+    public Flux<AdvisedResponse> aroundStream(AdvisedRequest advisedRequest, StreamAroundAdvisorChain chain) {
+        return chain.nextAroundStream(this.before(advisedRequest));
+    }
+
+    @Override
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+    private AdvisedRequest before(AdvisedRequest advisedRequest){
+        Map<String,Object> advisedUserParams = new HashMap<>(advisedRequest.userParams());
+        advisedUserParams.put("re2_put_query",advisedRequest.userText());
+
+        return AdvisedRequest.from(advisedRequest)
+                .userText("""
+                        {re2_input_query}
+                        Read the question again: {re2_input_query} 
+                        """)
+                .userParams(advisedUserParams)
+                .build();
+    }
+}
