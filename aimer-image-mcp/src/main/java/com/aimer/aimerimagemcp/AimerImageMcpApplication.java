1a0bf47cd2097d177c7dff5d5cd7dc1e89090764
chenxingyong <2103095120@qq.com>
2026-04-11 10:21:35 +0800

    feat: 添加 aimer-image-mcp 子模块

---
+++ aimer-image-mcp/src/main/java/com/aimer/aimerimagemcp/AimerImageMcpApplication.java
@@ -0,0 +1,13 @@
+package com.aimer.aimerimagemcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimerImageMcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AimerImageMcpApplication.class, args);
+    }
+
+}
---
+++ aimer-image-mcp/src/test/java/com/aimer/aimerimagemcp/AimerImageMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimer.aimerimagemcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimerImageMcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
